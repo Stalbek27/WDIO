f0736b3eabdec633ea9ca50f14843ea902531947
Stalbek <mambetaly.s@gmail.com>
2022-07-14 11:02:07 +0600

    home work 6

--- test/specs/home work 5.js
+++ test/specs/home work 5.js
@@ -1,14 +1,13 @@
-describe("Add/Remove Element", () => {
-    it("open page", async () => {
-        await browser.url("https://the-internet.herokuapp.com/add_remove_elements/");
-    });
+// describe("Add/Remove Element", () => {
+//     it("open page", async () => {
+//         await browser.url("https://the-internet.herokuapp.com/add_remove_elements/");
+//     });
 
-    it("click on button", async () => {
-        await $("#content > div > button").click();
-    });
+//     it("click on button", async () => {
+//         await $("#content > div > button").click();
+//     });
 
-    it("delet button", async () => {
-        await $("#elements > button").click();
-    });
-});
-    
+//     it("delet button", async () => {
+//         await $("#elements > button").click();
+//     });
+// });
