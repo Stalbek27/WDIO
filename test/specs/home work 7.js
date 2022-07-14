20abc75494c860cc7f20643d5fc722ba8428457a
Stalbek <mambetaly.s@gmail.com>
2022-07-14 15:30:48 +0600

    new update home work7

--- test/specs/home work 7.js
+++ test/specs/home work 7.js
@@ -1,17 +1,24 @@
 describe('Data tables', () => {
         it('data tables open page', async () => {
             await browser.url(`https://the-internet.herokuapp.com/tables`);
-            await browser.pause (3000);
+            await browser.pause (2000);
         });
 
-        it('click on due', async () => {
+        it('click on due first', async () => {
             let element = await browser.$('#table1 > thead > tr').click();
-            await browser.pause (3000);
+            await browser.pause (2000);
+        });
+
+        it('click on due second', async () => {
+            let element = await browser.$('#table1 > thead > tr').click();
+            await browser.pause (2000);
         });
         
-        it('click on due', async () => {
-            let element = await browser.$('#table1 > thead > tr').click();
-            await browser.pause (3000);
+        it('text check', async () => {
+            let element = await browser.$('//*[@id="table1"]/tbody/tr[1]/td[4]')
+            let text = await element.getText();
+            expect(text).toBe('$50.00');
+            await browser.pause (2000);
         });
 });
 
